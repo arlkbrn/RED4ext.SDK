cfed0d12372f4345f1207d72842f788819f74517
Ariel Kabrun <ariel.kabrun@gmail.com>
2025-11-17 13:24:17 -0400

    Fix small typo

--- include/RED4ext/Detail/Addresses.hpp
+++ include/RED4ext/Detail/Addresses.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #if defined(_WIN32)
-#error Addresses are not available directly in the Windows build. Did you mean to include AddressesHashes.hpp?
+#error Addresses are not available directly in the Windows build. Did you mean to include AddressHashes.hpp?
 #elif defined(__APPLE__)
 #include <RED4ext/Detail/mac/Addresses.hpp>
 #else
